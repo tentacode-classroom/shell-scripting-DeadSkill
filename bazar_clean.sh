9ca9970f5d00f1f8ab5fae07574a82c2f2399b30
Yanisse YOUMI <yanisse.youmi@ynov.com>
2018-12-17 17:52:53 +0100

    bazarproject

--- bazar_clean.sh
+++ bazar_clean.sh
@@ -4,20 +4,15 @@ echo "Rangement du bazar en cours..."
 echo "-------"
 
 # Suppression du précédent dossier de fixtures
-rm -Rf ./fixtures
+#rm -Rf ./
 
 # Création des répertoires de fixtures
 mkdir -p ./bazar_clean/divers \
     ./bazar_clean/images \
     ./bazar_clean/videos
 
-# Création des sous répertoires
-
-
-# Copie de l'image
-cp ./fixtures/bazar/Bureau/secret/chaton.jpg ./fixtures/bazar/chaton.jpg \
 
 # Copie des Fichiers textes
-cp ./bazar/Bureau/secret/journal_intime.txt ./bazar_clean/divers \
-    ./bazar/Bureau/secret/videos.mp4 ./bazar_clean/videos
+cp -r ./bazar/Bureau/secret/journal_intime.txt ./bazar_clean/divers \
+    ./bazar/Bureau/secret/video.mp4 ./bazar_clean/videos
 
